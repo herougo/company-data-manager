8a84a1eb64339ee7df1e0e504e1f542cb69a81c5
herougo <herougo@gmail.com>
2025-05-02 13:48:26 -0400

    refactor: separated tailwind classes into groups with an object

--- frontend/src/components/styling-wrappers/BlueButton.tsx
+++ frontend/src/components/styling-wrappers/BlueButton.tsx
@@ -1,9 +1,15 @@
 import IntrinsicButton from '../intrinsic/IntrinsicButton';
 import withStyle from '../hoc/withStyle';
 
+const tailwindClasses = {
+    'colour': 'bg-blue-500 text-white hover:bg-blue-300 active:bg-blue-500',
+    'border': 'border-0 rounded-full',
+    'other': 'text-base whitespace-nowrap cursor-pointer '
+};
+
 const BlueButton = withStyle(
     IntrinsicButton,
-    'bg-blue-500 text-white text-base border-0 rounded-full whitespace-nowrap cursor-pointer hover:bg-blue-300 active:bg-blue-500'
+    Object.values(tailwindClasses).join(' ')
 );
 
 export default BlueButton;
